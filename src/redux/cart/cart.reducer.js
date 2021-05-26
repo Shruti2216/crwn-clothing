eb4df2f6e733ba98dbff7431faaee1a6139ce039
Shruti2216 <shrutiyeole227@gmail.com>
2021-05-26 17:08:00 +0530

    Done with remove item from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
 import CartActionTypes from './cart.type';
+import {addItemToCart, removeItemFromCart} from './cart.utils';
+
 
 const INITIAL_STATE = {
-   hidden: true
+   hidden: true,
+   cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +14,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                ...state,
                hidden: !state.hidden
             };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+                return {
+                  ...state,
+                  cartItems: removeItemFromCart(state.cartItems, action.payload)
+                };     
+        case  CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }       
         default:
             return state;      
     }
